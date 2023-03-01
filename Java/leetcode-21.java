3d719412fa2ef17c5120dc2ddde854af74379887
Tyraffe <nathanb1357@gmail.com>
2023-02-28 18:11:48 -0800

    Added LeetCode 21 solution

---
+++ Java/leetcode-21.java
@@ -0,0 +1,27 @@
+package Java;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {this.val = val;}
+    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {return list2;}
+        if (list2 == null) {return list1;}
+
+        ListNode finalList;
+        if (list1.val < list2.val) {
+            finalList = list1;
+            finalList.next = mergeTwoLists(list1.next, list2);
+        } else {
+            finalList = list2;
+            finalList.next = mergeTwoLists(list2.next, list1);
+        }
+
+        return finalList;
+    }
+}
