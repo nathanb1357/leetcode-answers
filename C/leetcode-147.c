0757f6e3f14dacb34283963d6a0c672adf5f0cd2
Tyraffe <nathanb1357@gmail.com>
2023-03-01 00:31:25 -0800

    Added LeetCode 147 solution

---
+++ C/leetcode-147.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct ListNode {
+    int val;
+    struct ListNode *next;
+} ListNode;
+
+ListNode* insertionSortList(ListNode* head) {
+    ListNode* node = (ListNode*) malloc(sizeof(ListNode));
+    node->next = NULL;
+    ListNode* curr = head;
+    ListNode* prev = node;
+    ListNode* next;
+    while (curr) {
+        while (prev->next && prev->next->val < curr->val) {prev = prev->next;}
+        next = curr->next;
+		curr->next = prev->next;
+		prev->next = curr;
+		curr = next;
+		prev = node;
+    }
+    return node->next;
+}
