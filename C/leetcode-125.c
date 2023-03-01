a4b18e2dabf020a7ba232df8003a1681b5eb7cf3
Tyraffe <nathanb1357@gmail.com>
2023-02-28 18:29:57 -0800

    Added LeetCode 125 solution

---
+++ C/leetcode-125.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+bool isPalindrome(char* s) {
+    int i = 0;
+    int j = strlen(s) - 1;
+
+    while (j > i) {
+        while (!isalnum(s[i]) && j > i) {i++;}
+        while (!isalnum(s[j]) && j > i) {j--;}
+        s[i] = toupper(s[i]);
+        s[j] = toupper(s[j]);
+
+        if (s[i] != s[j]) {return 0;}
+        j--;
+        i++;
+    }
+    
+    return 1;
+}
